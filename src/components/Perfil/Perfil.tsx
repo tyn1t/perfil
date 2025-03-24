dba18e33e7a3e508ce1e447e0e167a8f63513c24
Giovanni <anderlansouza71@gmail.com>
2025-03-24 04:32:40 -0400

    image

--- src/components/Perfil/Perfil.tsx
+++ src/components/Perfil/Perfil.tsx
@@ -9,7 +9,7 @@ export function Perfil () {
                 <div className="absolute  rounded-l-full md:w-1/2 md:h-60 bg-amber-300"></div>
                 <div className="relative  md:right-10 w-64  h-56 -z-30 md:z-0 rounded-full  overflow-hidden">
                     <Image 
-                        src={`${apiUrl}circle-user-solid.svg`}
+                        src={`${apiUrl}/circle-user-solid.svg`}
                         alt="Foto de Perfil"
                         fill
                         className="object-contain"
